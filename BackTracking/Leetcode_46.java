b0472dd87b64849db6bf947b85439f37be05faa1
Subhrajit Deka <dekasubhrajit2007@gmail.com>
2026-07-27 17:57:14 +0530

    Commiting

--- BackTracking/Leetcode_46.java
+++ BackTracking/Leetcode_46.java
@@ -3,41 +3,36 @@ package BackTracking;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Leetcode_46
-{
-    class Solution
-    {
-        public List<List<Integer>> permute(int[] nums)
-        {
+public class Leetcode_46 {
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
             List<Integer> ls = new ArrayList<>();
             List<List<Integer>> ans = new ArrayList<>();
-            permutation( nums , ls, ans );
+            permutation(nums, ls, ans);
             return ans;
         }
 
-        public void permutation(int [] nums , List<Integer> ls , List<List<Integer>> ans )
-        {
-            if( ls.size() == nums.length )
-            {
+        public void permutation(int[] nums, List<Integer> ls, List<List<Integer>> ans) {
+            if (ls.size() == nums.length) {
                 List<Integer> s = new ArrayList<>(ls);
                 ans.add(s);
                 return;
             }
             int temp = 0;
-            for ( int i = 0 ; i < nums.length ; i++ )
-            {
+            for (int i = 0; i < nums.length; i++) {
 
-                if( nums[i]!=11 )
-                {
-                    ls.add( nums[i] );
+                if (nums[i] != 11) {
+                    ls.add(nums[i]);
                     temp = nums[i];
-                    nums[i]= 11;
-                    permutation(nums, ls,ans);
+                    nums[i] = 11;
+                    permutation(nums, ls, ans);
                     nums[i] = temp;
-                    ls.remove(ls.size()-1);
+                    ls.remove(ls.size() - 1);
                 }
 
             }
         }
-
+    }
 }
+
+
